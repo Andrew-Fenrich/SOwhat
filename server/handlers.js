122300468c158950cedb8cd5d27732d721a10d21
Andrew Fenrich <afenrich@gmail.com>
2021-04-13 19:50:49 -0400

    Back End complete

--- server/handlers.js
+++ server/handlers.js
@@ -47,6 +47,29 @@ const getUser = async (req, res) => {
   }
 };
 
+//function to soft delete User
+const deleteUser = async (req, res) => {
+  const client = await MongoClient(MONGO_URI, options);
+  const email = req.params.email;
+  try {
+    await client.connect();
+    const db = client.db();
+    console.log("connected!");
+    const user = await db.collection("users").findOne({ email: email });
+    const query = { email };
+    const newValues = { $set: { delete: !user.delete } };
+    await db.collection("users").updateOne(query, newValues);
+    if (user.delete === false) {
+      return res.status(202).json({ status: 202, message: "User Deleted" });
+    } else {
+      return res.status(202).json({ status: 202, message: "User reAdded" });
+    }
+  } catch (err) {
+    console.log(err.stack);
+    res.status(500).json({ status: 500, data: req.body, message: err.message });
+  }
+};
+
 // function to get a list of factors for analysis
 const getFactor = async (req, res) => {
   const client = await MongoClient(MONGO_URI, options);
@@ -118,6 +141,48 @@ const addSoWhat = async (req, res) => {
   }
 };
 
+// function to get all user SOwhats
+const getUserSOwhat = async (req, res) => {
+  const client = await MongoClient(MONGO_URI, options);
+  try {
+    await client.connect();
+    const db = client.db();
+    console.log("connected!");
+    const userId = req.params.userId;
+    const soWhats = await db.collection("SOwhats").find({ userId }).toArray();
+    client.close();
+    console.log("disconnected!");
+    return res.status(200).json({ status: 200, SOwhats: soWhats });
+  } catch (err) {
+    return res.status(500).json({ status: 500, message: err.message });
+  }
+};
+
+// function to delete a SOwhat
+
+const deleteSOwhat = async (req, res) => {
+  const client = await MongoClient(MONGO_URI, options);
+  let _id = req.params._id;
+  console.log(_id);
+  try {
+    await client.connect();
+    const db = client.db();
+    console.log("connected!");
+
+    const result = await db.collection("SOwhats").findOne({ _id });
+
+    await db.collection("SOwhats").deleteOne(result);
+
+    client.close();
+    console.log("disconnected!");
+
+    return res.status(204).json({ status: 204, message: "SOwhat Deleted" });
+  } catch (err) {
+    console.log(err.stack);
+    res.status(500).json({ status: 500, data: req.body, message: err.message });
+  }
+};
+
 module.exports = {
   getUsers,
   getUser,
@@ -125,4 +190,7 @@ module.exports = {
   addUser,
   addSoWhat,
   addFactor,
+  getUserSOwhat,
+  deleteSOwhat,
+  deleteUser,
 };
