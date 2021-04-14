d6e68065348f511d084ad5fd990e34037e2d224f
Andrew Fenrich <afenrich@gmail.com>
2021-04-13 20:15:01 -0400

    Browser Router, Switch and Router set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import styled from "styled-components";
 
 function App() {
   return (
-    <div>
-      <p>it starts</p>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          Dashboard
+        </Route>
+        <Route exact path="/sowhat">
+          So What
+        </Route>
+        <Route exact path="/settings">
+          Settings
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
