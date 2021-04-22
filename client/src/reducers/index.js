6aff4b3bd1f3c3a8c81cb63a0e476fb906e77a4f
Andrew Fenrich <afenrich@gmail.com>
2021-04-22 19:02:09 -0400

    Small code cleanup

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,5 @@
 import { combineReducers } from "redux";
 
-// import purchase from "./PurchaseReducer";
-// import item from "./ItemReducer";
 import user from "./UserReducer";
-// import cart from "./CartReducer";
 
 export default combineReducers({ user });
